d3389982d28fe5ab48adf939c929655c356cdc4c
blessedsibanda263 <blessedsibanda263@gmail.com>
2025-04-17 09:05:12 +0200

    Configure OAuth2 and OpenID Connect

--- go.mod
+++ go.mod
@@ -6,6 +6,10 @@ require (
 	github.com/coreos/go-oidc/v3 v3.14.1 // indirect
 	github.com/gin-contrib/sessions v1.0.3 // indirect
 	github.com/gin-gonic/gin v1.10.0 // indirect
+	github.com/go-jose/go-jose/v4 v4.0.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/pquerna/cachecontrol v0.2.0 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/oauth2 v0.29.0 // indirect
+	gopkg.in/go-jose/go-jose.v2 v2.6.3 // indirect
 )
---
+++ platform/authenticator/auth.go
@@ -0,0 +1,37 @@
+package authenticator
+
+import (
+	"context"
+	"os"
+
+	"github.com/coreos/go-oidc/v3/oidc"
+	"golang.org/x/oauth2"
+)
+
+// Authenticator is used to authenticate our users
+type Authenticator struct {
+	*oidc.Provider
+	oauth2.Config
+}
+
+// New instantiantes the authenticator
+func New() (*Authenticator, error) {
+	provider, err := oidc.NewProvider(
+		context.Background(),
+		"https://"+os.Getenv("AUTH0_DOMAIN")+"/",
+	)
+	if err != nil {
+		return nil, err
+	}
+	conf := oauth2.Config{
+		ClientID:     os.Getenv("AUTH0_CLIENT_ID"),
+		ClientSecret: os.Getenv("AUTH0_CLIENT_SECRET"),
+		RedirectURL:  os.Getenv("AUTH0_CALLBACK_URL"),
+		Endpoint:     provider.Endpoint(),
+		Scopes:       []string{oidc.ScopeOpenID, "profile"},
+	}
+	return &Authenticator{
+		Provider: provider,
+		Config:   conf,
+	}, nil
+}
